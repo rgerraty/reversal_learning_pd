7e2fa17b32a91ddd410ea544f4db39601e4c2c0d
rgerraty <rgerraty@gmail.com>
2016-03-08 22:04:20 -0500

    fingers crossed

--- KeyTest.m
+++ KeyTest.m
@@ -1,64 +1,31 @@
-function KeyTest(device,okResp,leftResp,rightResp,window)
-
-allowKeys=[okResp,leftResp,rightResp];
-endTime=0;
-outMC=0;
-while outMC==0
-    DrawFormattedText(window, 'Please click the INDEX finger button','center', 'center');
-    Screen('flip',window)
-    while 1
-        [buttonPress,pressTime] = KbQ_Func(device,allowKeys,endTime);
-        if strcmp(buttonPress,KbName(leftResp))
-            outMC=1;
-            Screen('flip',window)
-            WaitSecs(1.5)
-            break;
-        else
-            DrawFormattedText(window, 'Wrong Button','center', 'center');
-            Screen('flip',window)
-            WaitSecs(1)
-            break
-        end
-    end
-end
+function KeyTest(buttonBox,okResp,leftResp,rightResp,window)
 
 
-outMC=0;
-while outMC==0
-    DrawFormattedText(window, 'Please click the MIDDLE finger button','center', 'center');
-    Screen('flip',window)
-    while 1
-        [buttonPress,pressTime] = KbQ_Func(device,allowKeys,endTime);
-        if strcmp(buttonPress,KbName(rightResp))
-            outMC=1;
-            Screen('flip',window)
-            WaitSecs(1.5)
-            break;
-        else
-            DrawFormattedText(window, 'Wrong Button','center', 'center');
-            Screen('flip',window)
-            WaitSecs(1)
-            break
-        end
-    end
-end
+DrawFormattedText(window, 'Please click the INDEX finger button','center', 'center');
+Screen('flip',window)
+proc_key=zeros(1,256);
+proc_key(leftResp)=1;
+KbQueueCreate(buttonBox,proc_key);
+KbQueueStart(buttonBox);
+KbQueueWait(buttonBox);
+KbQueueFlush(buttonBox);
 
-outMC=0;
-while outMC==0
-    DrawFormattedText(window, 'Please click the THUMB button','center', 'center');
-    Screen('flip',window)
-    while 1
-        [buttonPress,pressTime] = KbQ_Func(device,allowKeys,endTime);
-        if strcmp(buttonPress,KbName(okResp))
-            outMC=1;
-            Screen('flip',window)
-            WaitSecs(1.5)
-            break;
-        else
-            DrawFormattedText(window, 'Wrong Button','center', 'center');
-            Screen('flip',window)
-            WaitSecs(1)
-            break
-        end
-    end
-end
+
+DrawFormattedText(window, 'Please click the MIDDLE finger button','center', 'center');
+Screen('flip',window)
+proc_key=zeros(1,256);
+proc_key(rightResp)=1;
+KbQueueCreate(buttonBox,proc_key);
+KbQueueStart(buttonBox);
+KbQueueWait(buttonBox);
+KbQueueFlush(buttonBox);
+
+
+DrawFormattedText(window, 'Please click the THUMB button','center', 'center');
+Screen('flip',window)
+proc_key=zeros(1,256);
+proc_key(okResp)=1;
+KbQueueCreate(buttonBox,proc_key);
+KbQueueStart(buttonBox);
+KbQueueWait(buttonBox);
+KbQueueFlush(buttonBox);
