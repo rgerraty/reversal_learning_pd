fc863d28b8c85affbbe00280e18f7b31ac864bc3
rgerraty <rgerraty@gmail.com>
2016-04-12 01:19:38 -0400

    code for restarting task after crash
    warning untested, will probably crash

--- ReversalTask_reboot.m
+++ ReversalTask_reboot.m
@@ -44,94 +44,114 @@ load(strcat(num2str(folder_name),'/inputP.mat'))
 trial=length(aq.chosenCat)
 
 %% Setting up the environment
-    rand('state',sum(100*clock));  % reset the state of rand to a random value
+rand('state',sum(100*clock));  % reset the state of rand to a random value
     
-    % Key Responses    
-    KbName('UnifyKeyNames');
-    escapeKey=KbName('q');
-    if scanned==2
-        leftResp=KbName('j');
-        rightResp=KbName('k');
-        okResp=KbName('space');
-        ttl=KbName('t');
-    else
-        leftResp=KbName('2@');
-        rightResp=KbName('3#');
-        okResp=KbName('1!');
-        ttl=KbName('t');
-    end
+% Key Responses    
+KbName('UnifyKeyNames');
+escapeKey=KbName('q');
+if scanned==2
+    leftResp=KbName('j');
+    rightResp=KbName('k');
+    okResp=KbName('space');
+    ttl=KbName('t');
+else
+    leftResp=KbName('2@');
+    rightResp=KbName('3#');
+    okResp=KbName('1!');
+    ttl=KbName('t');
+end
 
-    scenesDir='~/Documents/NETPD/StimuliPD/Aquisition/300Scenes/';
-    objectsDir='~/Documents/NETPD/StimuliPD/Aquisition/300Objects/';
+scenesDir='~/Documents/NETPD/StimuliPD/Aquisition/300Scenes/';
+objectsDir='~/Documents/NETPD/StimuliPD/Aquisition/300Objects/';
     
     
-    onsetlist=load(sprintf('pilotonsets_%d.mat',p.list));
-    onsetlist=onsetlist.onsetlist;
+onsetlist=load(sprintf('pilotonsets_%d.mat',p.list));
+onsetlist=onsetlist.onsetlist;
 
-    maxtime=2.5;
+maxtime=2.5;
 
-     img=cell(numel(aq.halfScenesList),2); % the stimuli converted to a screen texture for presentation later in script
-    for i=1:numel(aq.halfScenesList) % now size of objects and scenes arrays are both 1/2 the size
-        [o,~,alpha]=imread([scenesDir aq.halfScenesList(i).name], 'jpg');
-        StimCell=cat(3,o,alpha);
-        img{i,1}=Screen('MakeTexture',window, StimCell);
-        aq.scenes(i)=cellstr(aq.halfScenesList(i).name);        
-        [o,~,alpha]=imread([objectsDir aq.halfObjectsList(i).name], 'jpg');
-        StimRect=RectOfMatrix(o);
-        StimCell=cat(3,o,alpha);
-        img{i,2}=Screen('MakeTexture',window, StimCell);
-        aq.objects(i)=cellstr(aq.halfObjectsList(i).name);
-        Screen('TextSize',window, [30]);
-        Screen('TextStyle',window,[2]);
-        DrawFormattedText(window, ['Reading image #', num2str(i)], 'center','center', [0 0 0]); % temporary for coding purposes
-        Screen('Flip', window);
-    end  
-
-    StimRect=StimRect*(yPoints/6*3)./StimRect(3); %to make stim larger, need to fix after
+[window, windrect] = Screen('OpenWindow', 0); % get screen
+AssertOpenGL; % check for opengl compatability
+Screen('BlendFunction', window, GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);  %enables alpha blending for online image processing
+HideCursor;
     
-    StimX1=cx-360;
-    StimX2=cx+360;
+black = BlackIndex(window);  % Retrieves the CLUT color code for black.
+white=WhiteIndex(window);
+Screen('FillRect', window, white ); % Colors the entire window white.
     
-    StimBox1=CenterRectOnPoint(StimRect,StimX1,cy);
-    StimBox2=CenterRectOnPoint(StimRect,StimX2,cy);
-    StimBox1Frame=CenterRectOnPoint(StimRect*1.2,StimX1,cy);
-    StimBox2Frame=CenterRectOnPoint(StimRect*1.2,StimX2,cy);
+priorityLevel=MaxPriority(window);  % set priority
+Priority(priorityLevel);
+    
+Screen('TextSize', window, 36); %set text size
+Screen('TextColor', window, black);
+    
+[cx,cy]=RectCenter(windrect); %center point of screen
 
-    StimBox=CenterRectOnPoint([0 0 xPoints/4 xPoints/4],cx,cy);  % to squeeze the image to square, which is 1/4 of screen x-dim
-    StimBoxFrame=CenterRectOnPoint([0 0 xPoints/4 xPoints/4]*1.2,cx,cy);
-    DrawFormattedText(window, ['Images prepared.'], 'center','center', [0 0 0]);
-    Screen('Flip', window);
 
- Screen('TextSize',window, [50]);
+
+img=cell(numel(aq.halfScenesList),2); % the stimuli converted to a screen texture for presentation later in script
+    
+for i=1:numel(aq.halfScenesList) % now size of objects and scenes arrays are both 1/2 the size
+    [o,~,alpha]=imread([scenesDir aq.halfScenesList(i).name], 'jpg');
+    StimCell=cat(3,o,alpha);
+    img{i,1}=Screen('MakeTexture',window, StimCell);
+    aq.scenes(i)=cellstr(aq.halfScenesList(i).name);        
+    [o,~,alpha]=imread([objectsDir aq.halfObjectsList(i).name], 'jpg');
+    StimRect=RectOfMatrix(o);
+    StimCell=cat(3,o,alpha);
+    img{i,2}=Screen('MakeTexture',window, StimCell);
+    aq.objects(i)=cellstr(aq.halfObjectsList(i).name);
+    Screen('TextSize',window, [30]);
     Screen('TextStyle',window,[2]);
-    DrawFormattedText(window,['Which box has the points? \n\n Use the INDEX for Left \n use the MIDDLE finger for Right \n\n\n Press the THUMB to start'], 'center','center', [0 0 0]);
-    Screen('Flip', window); % show text
+    DrawFormattedText(window, ['Reading image #', num2str(i)], 'center','center', [0 0 0]); % temporary for coding purposes
+    Screen('Flip', window);
+end  
+
+StimRect=StimRect*(yPoints/6*3)./StimRect(3); %to make stim larger, need to fix after
     
-   if scanned==1 || scanned==2
-        DrawFormattedText(window,['Please wait while we prep the scanner\n\nExperimenter please press k when finished with prep scan'],'center','center',[0 0 0]);
-        Screen('Flip',window);
-        proc_key=zeros(1,256);
-        proc_key(KbName('k'))=1;
-        KbQueueCreate(kb,proc_key);
-        KbQueueStart(kb);
-        KbQueueWait(kb);
-        KbQueueFlush(kb);
+StimX1=cx-360;
+StimX2=cx+360;
+    
+StimBox1=CenterRectOnPoint(StimRect,StimX1,cy);
+StimBox2=CenterRectOnPoint(StimRect,StimX2,cy);
+StimBox1Frame=CenterRectOnPoint(StimRect*1.2,StimX1,cy);
+StimBox2Frame=CenterRectOnPoint(StimRect*1.2,StimX2,cy);
 
-        DrawFormattedText(window,['Please wait while we start the scan'],'center','center',[0 0 0]);
-        Screen('Flip',window);
-        keysofint=zeros(1,256);
-        keysofint(ttl)=1;
-        KbQueueCreate(trigger,keysofint);
-        KbQueueStart(trigger);
-        KbQueueWait(trigger);
+StimBox=CenterRectOnPoint([0 0 xPoints/4 xPoints/4],cx,cy);  % to squeeze the image to square, which is 1/4 of screen x-dim
+StimBoxFrame=CenterRectOnPoint([0 0 xPoints/4 xPoints/4]*1.2,cx,cy);
+DrawFormattedText(window, ['Images prepared.'], 'center','center', [0 0 0]);
+Screen('Flip', window);
 
-        DrawFormattedText(window,['Preparing the MRI...'],'center','center',[0 0 0]);
-        Screen('Flip',window);
-        WaitSecs(10.2)
-    end     
+Screen('TextSize',window, [50]);
+Screen('TextStyle',window,[2]);
+DrawFormattedText(window,['Which box has the points? \n\n Use the INDEX for Left \n use the MIDDLE finger for Right \n\n\n Press the THUMB to start'], 'center','center', [0 0 0]);
+Screen('Flip', window); % show text
+    
+if scanned==1 || scanned==2
+    DrawFormattedText(window,['Please wait while we prep the scanner\n\nExperimenter please press k when finished with prep scan'],'center','center',[0 0 0]);
+    Screen('Flip',window);
+    proc_key=zeros(1,256);
+    proc_key(KbName('k'))=1;
+    KbQueueCreate(kb,proc_key);
+    KbQueueStart(kb);
+    KbQueueWait(kb);
+    KbQueueFlush(kb);
 
-   	 startTime=datestr(now);
-    ExpStart=GetSecs;
+    DrawFormattedText(window,['Please wait while we start the scan'],'center','center',[0 0 0]);
+    Screen('Flip',window);
+    keysofint=zeros(1,256);
+    keysofint(ttl)=1;
+    KbQueueCreate(trigger,keysofint);
+    KbQueueStart(trigger);
+    KbQueueWait(trigger);
+
+    DrawFormattedText(window,['Preparing the MRI...'],'center','center',[0 0 0]);
+    Screen('Flip',window);
+    WaitSecs(10.2)
+end     
+
+startTime=datestr(now);
+ExpStart=GetSecs;
         
  
 escape=0;
@@ -527,7 +547,6 @@ while escape==0
     end
     Screen('CloseAll');
     escape=1;          %#ok<NASGU> % Exit after last trial
-catch
     Screen('CloseAll');
     %save(sprintf('%s/crashwork',folder_name),'crashWork'); %this saves the workspace (minus those cells) in event of a crash, for debugging
     ShowCursor;
