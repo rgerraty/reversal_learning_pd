2c0e4dd93e7d0c543026320ec623cb2a23463a88
rgerraty <rgerraty@lovelace.psych.columbia.edu>
2016-04-28 20:02:32 -0400

    convert, move, and reorient dicoms

---
+++ analysis/convert_dicoms.sh
@@ -0,0 +1,19 @@
+dicom_dir=$(readlink -f $1)
+
+if [[ -z $(ls $dicom_dir/*dcm) ]];
+	then 
+	echo no dcm files in $1
+elif [[ -z $dicom_dir/../nifti/*nii.gz ]]
+	then
+	echo nifti files already exist for $1
+	echo please check directory and delete nifti before continuing
+else 
+	cd $dicom_dir
+	echo converting dicoms in $dicom_dir
+	dcm2nii ./*
+	mv -p *nii.gz ../nifti/
+	for i in ../nifti/*nii.gz;
+	do
+		fslreorient2std $i;
+	done
+fi
