da6a36bc6d88ef1b721b6fbcf0a71856b7c3b131
rgerraty <rgerraty@gmail.com>
2016-04-29 15:14:09 -0400

    B0 unwarping

--- analysis/B0_unwarp.sh
+++ analysis/B0_unwarp.sh
@@ -1,26 +1,13 @@
 dir_name=$(dirname $(readlink -f $1))
 file_name=$(basename $1)
 
-echo converting to complex image from $1
+#note this will only work for very specific B0 images already in phase/magnitide 
+echo splitting $1 into phase and magnitude images
+fslsplit $1 $dir_name/vol
+mv $dir_name/vol0000.nii.gz $dir_name/fieldmap_rads.nii.gz
+mv $dir_name/vol0001.nii.gz $dir_name/fieldmap_mag.nii.gz
 
-cd $dir_name
-fslcomplex -complexsplit $file_name complex_acq
-
-fslcomplex -realabs complex_acq fieldmap_mag
-
-echo generating phase images
-fslcomplex -realphase complex_acq phase0_rad 0 1
-
-fslcomplex -realphase complex_acq phase1_rad 1 1
-
-echo unwrapping phase images
-prelude -a fieldmap_mag -p phase0_rad -o phase0_unwrapped_rad
-
-prelude -a fieldmap_mag -p phase1_rad -o phase1_unwrapped_rad
-
-echo generating field map
-fslmaths phase1_unwrapped_rad -sub phase0_unwrapped_rad -mul 1000 -div 2.65 fieldmap_rads -odt float 
-
-fugue --loadfmap=fieldmap_rads -s 1 --savefmap=fieldmap_rads
-fugue --loadfmap=fieldmap_rads --despike --savefmap=fieldmap_rads
-fugue --loadfmap=fieldmap_rads -m --savefmap=fieldmap_rads
+echo regularizing field map
+fugue --loadfmap=$dir_name/fieldmap_rads -s 1 --savefmap=$dir_name/fieldmap_rads
+fugue --loadfmap=$dir_name/fieldmap_rads --despike --savefmap=$dir_name/fieldmap_rads
+fugue --loadfmap=$dir_name/fieldmap_rads -m --savefmap=$dir_name/fieldmap_rads
